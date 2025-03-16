b6a8c8c802f365aa58b51e7259e0f67ed938d084
Wesley Senciani <wsa.wes05@gmail.com>
2025-03-16 10:03:56 -0300

    crating order routes for each crud controller

--- src/routes/order-routes.ts
+++ src/routes/order-routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express'
-import ProductController from '../controllers/product-controller'
 import OrderController from '../controllers/order-controller'
 
 const orderRoutes = Router()
 
 orderRoutes.get('/', OrderController.getOrderList)
+
+orderRoutes.get('/:id', OrderController.getOrderById)
+
+orderRoutes.post('/', OrderController.createOrder)
+
+orderRoutes.put('/:id',OrderController.updateOrder)
+
+orderRoutes.delete('/:id', OrderController.removeOrder)
 
 export default orderRoutes
