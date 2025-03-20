ec0f4d38318e2725ff61e3014e84a0a7a2c7360d
Caio Assis <caio.ab.assis@gmail.com>
2025-03-19 23:17:24 -0300

    Teste BD

--- src/routes/order-routes.ts
+++ src/routes/order-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import OrderController from '../controllers/order-controller'
-
+//rebranch
 const orderRoutes = Router()
 
 orderRoutes.get('/', OrderController.getOrderList)
